3955a41a7cea943c5303161295e1aa5510f5f24c
Nivas-git07 <itsmenivas007@gmail.com>
2025-08-09 18:49:52 +0000

    fix path

--- lib/server_logic.dart/server.dart
+++ lib/server_logic.dart/server.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:extrack/home.dart';
+import '../package:extrack/home.dart';
 
 Future<void> postUser(String name , String age) async{
   final url = Uri.parse("http://127.0.0.1:8000/signup");
