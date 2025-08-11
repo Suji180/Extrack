06fb51d4897cc441991b0662d0711723c24bff2d
Saravanesh-Git <saravanesh962006@gmail.com>
2025-08-11 21:25:50 +0530

    Receipt upload for expense adding

--- api/expense.py
+++ api/expense.py
@@ -1,13 +1,14 @@
-from fastapi import FastAPI, Request, Response, Depends, HTTPException, APIRouter
+from fastapi import FastAPI, Request, Response, Depends, HTTPException, APIRouter, UploadFile, File
 from pydantic import BaseModel
 import psycopg
+import os
 from basemodel import AddExpense
 from db import db_connection
 
 load = APIRouter()
 
 @load.post("/add")
-async def add_expense(add: AddExpense):
+async def add_expense(add: AddExpense, file: UploadFile = File(...)):
     conn = db_connection()
     cursor = conn.cursor()
     try:
@@ -21,3 +22,22 @@ async def add_expense(add: AddExpense):
     finally:
         cursor.close()
         conn.close()
+
+directory = "/home/saravanesh/receipts"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+@load.post("/image")
+async def upload(file: UploadFile = File(...)):
+    try:
+        file_location = os.path.join(directory, file.filename)
+        with open(file_location, "wb") as buffer:
+            content = await file.read()
+            buffer.write(content)
+        return {"Message": "Image uploaded"}
+
+    except Exception as e:
+        return HTTPException(status_code = 500, detail = f"The error is {e}")
+
+    finally:
+        pass
