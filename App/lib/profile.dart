00c700d1ed038121800031d60d9cb52f775cd7c9
Vashist <vashistvishwesh@gmail.com>
2025-08-23 21:49:42 +0530

    The login and signup button is added

--- App/lib/profile.dart
+++ App/lib/profile.dart
@@ -1,4 +1,7 @@
+import 'package:extrack/Login_sign_up_page/SignUP%20page.dart';
+import 'package:extrack/Login_sign_up_page/loginpage.dart';
 import 'package:flutter/material.dart';
+
 
 
 
@@ -7,9 +10,66 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(child: Text('Welcome to the Profile page !')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SecondPage(),));
+                },
+                child: Container(
+                  
+                  padding: const EdgeInsets.all(10),
+                  height: MediaQuery.of(context).size.height*0.1,
+                  width: MediaQuery.of(context).size.width*0.5,
+                  
+                  child: Center(
+                    child: const Text('Login'
+                    ,style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 26, 95, 135)
+                    ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: const Color.fromARGB(255, 202, 203, 203)),
+                  margin: const EdgeInsets.all(20),
+                  
+                ),
+              ),
+            ),
+
+             Center(
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(context,MaterialPageRoute(builder: (context)=> SignUpPage()));
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  height: MediaQuery.of(context).size.height*0.1,
+                  width: MediaQuery.of(context).size.width*0.5,
+                  child: Center(
+                    child: const Text('Sign Up'
+                    ,style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 26, 95, 135)
+                    ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: const Color.fromARGB(255, 202, 203, 203)),
+                  margin: const EdgeInsets.all(20),
+                  
+                ),
+              ),
+            )
+                
+          ],
+        )
 
         
       )
