d92b31b967d247242987b821ee5c5f4a3b3144bc
Saravanesh-Git <saravanesh962006@gmail.com>
2025-09-22 21:36:02 +0530

    New branch created name : frontend

--- App/lib/server_logic.dart/server.dart
+++ App/lib/server_logic.dart/server.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'dart:io';
 
-
+//New bracnh created name : frontend
 
 Future<void> postUser(String name , String age) async{
   final url = Uri.parse("http://10.0.2.2:8000/signup");
