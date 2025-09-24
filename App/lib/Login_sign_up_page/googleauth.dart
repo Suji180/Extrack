11c45523a1a1ac6078301e59822ca8858d31c253
Vashist <vashistvishwesh@gmail.com>
2025-09-24 20:30:49 +0530

    For project purpose

--- App/lib/Login_sign_up_page/googleauth.dart
+++ App/lib/Login_sign_up_page/googleauth.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/cupertino.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:google_sign_in/google_sign_in.dart'; 
 import 'package:extrack/server_logic.dart/server.dart';
 
 
